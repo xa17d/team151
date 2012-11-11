63df97c9111e7ae1db9998f78c2e0ba7f1ff1212
daniel <daniel.gehrer@gmx.net>
2012-11-11 15:14:29 +0100

    FreeBox implementiert und Testfaelle hinzugefuegt

--- BoxManager/src/Test.java
+++ BoxManager/src/Test.java
@@ -9,7 +9,6 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		/*
 		//BOX
 		Box b = new Box(3.4, 2.4, 'o', '-');
 		System.out.println(b.toString());
@@ -32,8 +31,7 @@ public class Test {
 		System.out.println(db.toString());
 		db.scale(0.5);
 		System.out.println(db.toString());
-		*/
-		
+
 		// Repeated
 		Repeated<String> repeated1 = new Repeated<String>(
 
@@ -54,6 +52,15 @@ public class Test {
 		System.out.println("*** Repeated 1 Skaliert 0.5*2.5 = 1.25 ***");
 		repeated1.scale(2.5);
 		System.out.println(repeated1.toString());
+		
+		// FreeBox
+		FreeBox freeBox1 = new FreeBox("1234\n5678");
+		System.out.println("*** Freebox 1 ***");
+		System.out.println(freeBox1.toString());
+		
+		System.out.println("*** Freebox 1 Skaliert 1.5***"); // (Beispiel aus der Angabe)
+		freeBox1.scale(1.5);
+		System.out.println(freeBox1.toString());
 	}
 
 }
