1bac4a467a595ac1716a2263d6d559a5cf433e74
rainer-hohn <e1125579@student.tuwien.ac.at>
2012-11-13 20:13:02 +0100

    Ausgabe mit Text uebersichtlicher gestaltet

--- BoxManager/src/Test.java
+++ BoxManager/src/Test.java
@@ -10,26 +10,24 @@ public class Test {
 	public static void main(String[] args) {
 		//BOX
 		Box b = new Box(3.4, 2.4, 'o', '-');
-		System.out.println(b.toString());
+		System.out.println("*** Ausgabe Box ***\n" + b.toString());
 		b.scale(0.5);
 		b.scale(0.5);
-		b.scale(0.5);
-		b.scale(0.5);
-		System.out.println(b.toString());
+		System.out.println("*** Ausgabe Box nach 2 x scale(0.5) ***\n" + b.toString());
 		
 		//CELAR BOX
 		ClearBox cb = new ClearBox(4, 4);
-		System.out.println(cb.toString());
-		System.out.println(cb.delta());
+		System.out.println("*** Ausgabe Clear-Box ***\n" + cb.toString());
+		System.out.println("*** Ausgabe delta von Clear-Box ***\n" + cb.delta());
 		cb.scale(0.5);
 		cb.scale(0.5);
-		System.out.println(cb.toString());
+		System.out.println("*** Ausgabe Clear-Box nach 2 x scale(0.5) ***\n" + cb.toString());
 		
 		//DARK BOX
 		DarkBox db = new DarkBox(6, 6, '#');
-		System.out.println(db.toString());
+		System.out.println("*** Ausgabe Dark-Box ***\n" + db.toString());
 		db.scale(0.5);
-		System.out.println(db.toString());
+		System.out.println("*** Ausgabe Dark-Box nach scale(0.5) ***\n" + db.toString());
 
 		//Repeated
 		String[][] repeatedString = new String[][] {
