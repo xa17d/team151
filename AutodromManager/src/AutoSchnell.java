ead901cddf88c648cc74645d08245a3b2ac77c93
daniel <daniel.gehrer@gmx.net>
2012-11-30 13:31:10 +0100

    Bewegliches Auto

--- AutodromManager/src/AutoSchnell.java
+++ AutodromManager/src/AutoSchnell.java
@@ -1,4 +1,7 @@
-
+/**
+ * Schnelles Auto. Bewegt sich in einem Interval von 100ms
+ * @author Daniel
+ */
 public class AutoSchnell extends Auto {
 	public AutoSchnell(Feld feld, Position anfangsPosition) {
 		super(feld, 100, anfangsPosition);
@@ -6,6 +9,6 @@ public class AutoSchnell extends Auto {
 
 	@Override
 	protected Position bewegen(Position b) {
-		return b.links();
+		return b.halbRechts();
 	}
 }
