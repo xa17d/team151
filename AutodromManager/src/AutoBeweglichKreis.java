a0456488402c37f5e1576664fede03e75143ce6e
Lucas <lucas.dobler@gmail.com>
2012-12-04 15:10:45 +0100

    kommentare zu kreisautos

--- AutodromManager/src/AutoBeweglichKreis.java
+++ AutodromManager/src/AutoBeweglichKreis.java
@@ -1,19 +1,31 @@
 /**
- * 
+ * implementiert ein bewegliches Auto, das immer im Kreis faehrt
+ * hat die Geschwindigkeit eines beweglichen Autos
  * @author Lucas
- *
  */
 public class AutoBeweglichKreis extends AutoBeweglich {
 	int bewegungen;
 	
+	/**
+	 * Konstruktor des beweglichen Autos, das im Kreis faehrt
+	 * @param feld Feld auf dem es fahren soll
+	 * @param anfangsPosition Startposition (!null)
+	 */
 	public AutoBeweglichKreis(Feld feld, Position anfangsPosition) {
 		super(feld, anfangsPosition);
 		
 		bewegungen = 0;
 	}
 	
+	/**
+	 * Wird im festgelegten Intervall aufgerufen.
+	 * Das Auto faehr im Kreis
+	 * @param p Aktuelle Position und Richtung des Autos (niemals null)
+	 * @return Position und Richtung des Autos nach diesem Bewegungsschritt (niemals null)
+	 */
 	@Override
 	protected Position bewegen(Position b) {
+		//0 <= bewegungen <= 20
 		bewegungen++;
 		
 		if(bewegungen == 5 || bewegungen == 10 || bewegungen == 15)
--- AutodromManager/src/AutoSchnellKreis.java
+++ AutodromManager/src/AutoSchnellKreis.java
@@ -1,20 +1,32 @@
 /**
- * 
+ * implementiert ein schnelles Auto, das immer im Kreis faehrt
+ * hat die Geschwindigkeit eines schnellen Autos
  * @author Lucas
- *
  */
 public class AutoSchnellKreis extends AutoSchnellZufall {
 	int bewegungen;
 	
+	/**
+	 * Konstruktor des schnellen Autos, das im Kreis faehrt
+	 * @param feld Feld auf dem es fahren soll
+	 * @param anfangsPosition Startposition (!null)
+	 */
 	public AutoSchnellKreis(Feld feld, Position anfangsPosition) {
 		super(feld, anfangsPosition);
-		
+
 		bewegungen = 0;
 	}
 	
+	/**
+	 * Wird im festgelegten Intervall aufgerufen.
+	 * Das Auto faehr im Kreis
+	 * @param p Aktuelle Position und Richtung des Autos (niemals null)
+	 * @return Position und Richtung des Autos nach diesem Bewegungsschritt (niemals null)
+	 */
 	@Override
 	protected Position bewegen(Position b) {
-		bewegungen++;
+		//0 <= bewegungen <= 16
+		bewegungen++;		
 		
 		if(bewegungen == 3 || bewegungen == 4 || bewegungen == 7 || bewegungen == 8 || bewegungen == 11 || bewegungen == 12 || bewegungen == 15)
 			return b.halbRechts();
