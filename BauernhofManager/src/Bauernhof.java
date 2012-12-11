0873592cde70f3df1c4a05b7fcff201a791a46b1
daniel <daniel.gehrer@gmx.net>
2012-12-11 17:58:43 +0100

    Kommentare

--- BauernhofManager/src/Bauernhof.java
+++ BauernhofManager/src/Bauernhof.java
@@ -53,41 +53,105 @@ public class Bauernhof implements IteratableObject {
 		return traktors.get(id);
 	}
 	
+	/**
+	 * Gibt den Namen des Bauernhofs zurueck
+	 */
 	@Override
 	public String toString() {
 		return name;
 	}
 
+	/**
+	 * @see IteratableObject#iteration(Iteration)
+	 */
 	@Override
 	public void iteration(Iteration iteration) {
 		iteration.iterationBauernhof(this);		
 	}
 	
-	private double berechneAvgBetiebsstunden(Iteration iteration, IterationAvgBetriebsstunden betriebsstunden) {
+	private double berechneAvg(Iteration iteration, IterationAvgCounter avg) {
 		traktors.iterate(iteration);
-		return betriebsstunden.getAvg();
+		return avg.getAvg();
 	}
 	
 	public double berechneAvgBetiebsstunden()  {
 		IterationAvgBetriebsstunden i = new IterationAvgBetriebsstunden();
-		return berechneAvgBetiebsstunden(i,i);
+		return berechneAvg(i,i);
 	}
 	
 	public double berechneAvgBetiebsstundenSaeen() {
 		IterationAvgBetriebsstunden i = new IterationAvgBetriebsstunden();
-		return berechneAvgBetiebsstunden(new Filters.ModulDrillmaschine(i),i);
+		return berechneAvg(new Filters.ModulDrillmaschine(i),i);
 	}
 	public double berechneAvgBetiebsstundenDuengen() {
 		IterationAvgBetriebsstunden i = new IterationAvgBetriebsstunden();
-		return berechneAvgBetiebsstunden(new Filters.ModulDuengerstreuer(i),i);
+		return berechneAvg(new Filters.ModulDuengerstreuer(i),i);
 	}
 	public double berechneAvgBetiebsstundenDiesel() {
 		IterationAvgBetriebsstunden i = new IterationAvgBetriebsstunden();
-		return berechneAvgBetiebsstunden(new Filters.Diesel(i),i);
+		return berechneAvg(new Filters.Diesel(i),i);
 	}
-	public double berechneAvgBetiebsstundenBiogas() {
-		IterationAvgBetriebsstunden i = new IterationAvgBetriebsstunden();
-		return berechneAvgBetiebsstunden(new Filters.Biogas(i),i);
+	public double berechneAvgDieselverbrauch() {
+		IterationAvgDieselverbrauch i = new IterationAvgDieselverbrauch();
+		return berechneAvg(i,i);
+	}
+	public double berechneAvgDieselverbrauchSaeen() {
+		IterationAvgDieselverbrauch i = new IterationAvgDieselverbrauch();
+		return berechneAvg(new Filters.ModulDrillmaschine(i),i);
+	}
+	public double berechneAvgDieselverbrauchDuengen() {
+		IterationAvgDieselverbrauch i = new IterationAvgDieselverbrauch();
+		return berechneAvg(new Filters.ModulDuengerstreuer(i),i);
+	}
+	public double berechneAvgGasverbrauchSaeen() {
+		IterationAvgGasverbrauch i = new IterationAvgGasverbrauch();
+		return berechneAvg(new Filters.ModulDrillmaschine(i),i);
+	}
+	public double berechneAvgGasverbrauchDuengen() {
+		IterationAvgGasverbrauch i = new IterationAvgGasverbrauch();
+		return berechneAvg(new Filters.ModulDuengerstreuer(i),i);
+	}
+	public double berechneAvgFassungskapazitaet() {
+		IterationAvgFassungsvermoegen i = new IterationAvgFassungsvermoegen();
+		return berechneAvg(i, i);
+	}
+	public double berechneAvgFassungskapazitaetDiesel() {
+		IterationAvgFassungsvermoegen i = new IterationAvgFassungsvermoegen();
+		return berechneAvg(new Filters.Diesel(i), i);
+	}
+	public double berechneAvgFassungskapazitaetBiogas() {
+		IterationAvgFassungsvermoegen i = new IterationAvgFassungsvermoegen();
+		return berechneAvg(new Filters.Biogas(i), i);
+	}
+	public double berechneMinAnzahlSaescharen() {
+		IterationAnzahlSaescharen i = new IterationAnzahlSaescharen();
+		traktors.iterate(i);
+		return i.getMin();
+	}
+	public double berechneMaxAnzahlSaescharen() {
+		IterationAnzahlSaescharen i = new IterationAnzahlSaescharen();
+		traktors.iterate(i);
+		return i.getMax();
+	}
+	public double berechneMinAnzahlDiesel() {
+		IterationAnzahlSaescharen i = new IterationAnzahlSaescharen();
+		traktors.iterate(new Filters.Diesel(i));
+		return i.getMin();
+	}
+	public double berechneMaxAnzahlDiesel() {
+		IterationAnzahlSaescharen i = new IterationAnzahlSaescharen();
+		traktors.iterate(new Filters.Diesel(i));
+		return i.getMax();
+	}
+	public double berechneMinAnzahlBiogas() {
+		IterationAnzahlSaescharen i = new IterationAnzahlSaescharen();
+		traktors.iterate(new Filters.Biogas(i));
+		return i.getMin();
+	}
+	public double berechneMaxAnzahlBiogas() {
+		IterationAnzahlSaescharen i = new IterationAnzahlSaescharen();
+		traktors.iterate(new Filters.Biogas(i));
+		return i.getMax();
 	}
 	
 	private class IterationAvgCounter extends Iteration {
@@ -114,6 +178,61 @@ public class Bauernhof implements IteratableObject {
 			// TODO:
 			//count(item.getBetriebsstunden());
 			count(1);
+		}
+	}
+	
+	private class IterationAvgDieselverbrauch extends IterationAvgCounter {
+		@Override
+		public void iterationTraktorDiesel(TraktorDiesel item) {
+			// TODO:
+			//count(item.getDieselverbrauch());
+			count(1);
+		}
+	}
+	
+	private class IterationAvgGasverbrauch extends IterationAvgCounter {
+		@Override
+		public void iterationTraktorBiogas(TraktorBiogas item) {
+			// TODO:
+			//count(item.getGasverbrauch());
+			count(1);
+		}
+	}
+	
+	private class IterationAvgFassungsvermoegen extends IterationAvgCounter {
+		@Override
+		public void iterationModulDuengerstreuer(Duengerstreuer modul, Traktor traktor) {
+			count(modul.getFassungskapazitaet());
+		}
+	}
+	
+	private class IterationAnzahlSaescharen extends Iteration {
+		private int min = Integer.MAX_VALUE;
+		private int max = Integer.MIN_VALUE;
+		private boolean valid = false;
+		
+		@Override
+		public void iterationModulDrillmaschine(Drillmaschine modul, Traktor traktor) {
+			int c = modul.getAnzahlSaehscharen();
+			if (c < min) { min = c; }
+			if (c > max) { max = c; }
+			valid = true;
+		}
+		
+		public int getMin() {
+			if (valid) {
+				return min;
+			} else {
+				return 0;
+			}
+		}
+		
+		public int getMax() {
+			if (valid) {
+				return max;
+			} else {
+				return 0;
+			}
 		}
 	}
 }
--- BauernhofManager/src/Traktor.java
+++ BauernhofManager/src/Traktor.java
@@ -6,13 +6,24 @@ public abstract class Traktor implements IteratableObject {
 	private Modul modul;
 	public Modul getModul() { return modul; }
 	
+	/**
+	 * Ruft iteration.iterationTraktor auf. Auch Untertypen von Traktor rufen genau diese Methode auf
+	 */
 	@Override
 	public final void iteration(Iteration iteration) {
 		iteration.iterationTraktor(this);
 	}
 	
+	/**
+	 * Ruft je nach Untertyp iteration.iterationTraktorBiogas oder iteration.iterationTraktorDiesel auf
+	 * @param iteration Iteration
+	 */
 	public abstract void interationTyp(Iteration iteration);
 	
+	/**
+	 * Ruf je nach Modul iteration.iterationModulDrillmaschine oder iteration.iterationModulDuengerstreuer auf
+	 * @param iteration Iteration
+	 */
 	public void interationModul(Iteration iteration) {
 		modul.interationModul(iteration, this);
 	}
